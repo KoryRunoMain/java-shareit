c366520d7660326975478af3efbb121f9654674b
koryruno <koryruno@yandex.ru>
2024-04-26 10:37:54 +0300

    fix:

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -6,12 +6,8 @@ import java.util.List;
 
 public interface UserService {
     UserDto create(UserDto userDto);
-
     UserDto update(UserDto userDto, Long id);
-
     UserDto delete(Long userId);
-
     UserDto get(Long userId);
-
     List<UserDto> getUsers();
 }
